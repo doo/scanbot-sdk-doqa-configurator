ead5c88459e4bb46852767ef259a58c8fe13424e
Benjamin Klotz <benjamin@scanbot.io>
2025-07-15 12:00:39 +0200

    Fix problems with docker container

--- src/train_docker.py
+++ src/train_docker.py
@@ -18,7 +18,7 @@ import OpenCVSVMClassifier
 @click.command(context_settings={'show_default': True})
 @click.option('--scanbotsdk_license_key', type=str, required=True, help='Scanbot SDK license key')
 @click.option('--training_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path),
-              default=Path(os.path.dirname(os.path.realpath(__file__))) / ".." / "data",
+              default=Path(__file__).parent.parent / "data",
               help='Directory containing training images in the subfolders "good" and "bad"')
 @click.option('--num_jobs', type=int, default=4, help='Number of parallel jobs for training')
 @click.option('--smoke_test', is_flag=True, help='Run a smoke test with a small subset of the data')
